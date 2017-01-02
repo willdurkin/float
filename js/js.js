a449a46a18a89cd0335ed20373f6a1e478d5199f
Will Durkin <willdurkin@gmail.com>
2017-01-02 00:12:31 -0500

    d10

--- js/js.js
+++ js/js.js
@@ -22,29 +22,15 @@ $(document).ready(function() {
 	// LocalScroll //
 	
 	jQuery(function( $ ){
-        /**
-         * Most jQuery.localScroll's settings, actually belong to jQuery.ScrollTo, check it's demo for an example of each option.
-         * @see http://flesler.demos.com/jquery/scrollTo/
-         * You can use EVERY single setting of jQuery.ScrollTo, in the settings hash you send to jQuery.LocalScroll.
-         */
-        
-        // The default axis is 'y', but in this demo, I want to scroll both
-        // You can modify any default like this
         $.localScroll.defaults.axis = 'yx';
-        
-        // Scroll initially if there's a hash (.something) in the url 
         $.localScroll.hash({
-            target: '#content', // Could be a selector or a jQuery object too.
+            target: '#content', 
             queue:true,
             duration:1000,
         });
         
-        /**
-         * NOTE: I use $.localScroll instead of $('#navigation').localScroll() so I
-         * also affect the >> and << links. I want every link in the page to scroll.
-         */
         $.localScroll({
-            target: '#content', // could be a selector or a jQuery object too.
+            target: '#content', 
             queue:true,
             duration:1500,
             hash:true,
@@ -479,16 +465,25 @@ $(document).ready(function() {
 		$d10prev.fadeOut(2000);
 		$d10special.effect( "bounce", { distance: 7, times: 3}).fadeOut(2000);
 		$d10special2.delay(2500).fadeIn(2000)
-
 	})
 
+//d10
 	
-	
 
-	// c10 //
+	//d10cont
 
-
-
+	$b10prev1 = $('#b10cont a')
+	$b10div = $('#b10cont div')
+	$b10prev1.click(function(){
+		$(this).fadeOut(2000);
+		$('#b10cont').css('pointer-events', 'none')
+		$b10div
+			.fadeIn(2000)
+			.delay(2000)
+			.fadeOut(3000);
+		$('#box').delay(5000).fadeIn(3000);
+		
+	})
 
 	// CARL //
 
